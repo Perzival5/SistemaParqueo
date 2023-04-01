d8cbddb7e13abc9a2356ec7cb910a9ea6dcdcee7
rimer saul <rimersaulrosaherrera@gmail.com>
2023-03-31 21:59:11 -0400

    navegador

---
+++ src/components/espacio/espacio.js
@@ -0,0 +1,344 @@
+import React from 'react';
+import logo from '../assets/Verde.png'
+import './espacio.css'
+const Espacios = () => {
+  return (
+    <body>
+      <div class="contenedor">
+        <br/>   
+        <h1 >Estado del parqueo</h1>
+      </div>
+      <br />
+      <div class='grid-container'>
+        <h1></h1>
+        <h1>A</h1>
+        <h1>B</h1>
+        <h1>C</h1>
+        <h1>D</h1>
+        <h1>E</h1>
+        <h1>F</h1>
+        <h1>G</h1>
+        <h1>H</h1>
+        <h1>I</h1>
+        <h1>J</h1>
+        <h1>K</h1>
+        <h1>L</h1>
+        <h1>1</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <h1>2</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+      </div>
+      
+      <div class='grid-container'>
+      <h1>3</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <h1>4</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+      </div>
+     
+      <div class='grid-container'>
+      <h1>5</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <h1>6</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+      </div>
+     
+      <div class='grid-container'>
+      <h1>7</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <h1>8</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+      </div>
+ 
+      <div class='grid-container'>
+      <h1>9</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <h1>10</h1>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+        <div className='image'><div>a</div></div>
+      </div>
+    </body>
+
+  );
+};
+
+export default Espacios;
+
+/*import logo from './assets/Verde.png';
+import './App.css';
+
+function App() {
+  /*  
+  const items = llamada ala base;
+  */
+/* return (
+   <div className="App">
+     
+     <body><ul class="showcase">
+     <li>
+       <div class="seat"></div>
+       <small>N/A</small>
+     </li>
+
+     <li>
+       <div class="seat selected"></div>
+       <small>Selecionado</small>
+     </li>
+
+     <li>
+       <div class="seat occupied"></div>
+       <small>Ocupado</small>
+     </li>
+     </ul>
+   
+     <div className='grid-container'>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     <div className='image'><img src = {logo}/><div>a</div></div>
+     
+       
+     </div>
+   
+   
+</body>
+   </div>
+   
+ );
+}
+
+export default App;
+<script src="script.js"></script>
+
+ {items.map((itme)=>(
+   <div key = {item.id} className='grid-item'>
+   <img src = {"https://image.tmdb.org/t/p/w150" + item.path;}/>
+   {item.lugar}
+   </div>
+ ))}
+ */
