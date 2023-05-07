c4c6ae2ebafe68b686561729c6ed99df331b910c
rimer rosa herrera <201809665@est.umss.edu>
2023-05-07 02:00:37 -0400

    listo ponde a dar maquillaje

--- src/components/espacio/espacio.js
+++ src/components/espacio/espacio.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Calendar from 'react-calendar';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datetime/css/react-datetime.css';
 import './espacio.css'
+import axios from 'axios';
+
+
 
 function mostrarModal() {
   document.getElementById("miModal").style.display = "block";
@@ -11,8 +17,40 @@ function cerrarModal() {
 }
 
 const Espacios = () => {
+  const [nombre, setNombre] = useState('');
+  const [apellidos, setApellidos] = useState('');
+  const [ci, setCI] = useState('');
+  const [rol, setRol] = useState('');
+  const [celular, setCelular] = useState('');
+  const [correo, setCorreo] = useState('');
+  const [placa, setPlaca] = useState('');
+  const [marca, setMarca] = useState('');
+  const [modelo, setModelo] = useState('');
+  const [tipo, setTipo] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  
+  const [Modal2, setModal2] = useState(false);
   const [value, onChange] = useState(new Date());
   const [selectedDate, setSelectedDate] = useState(null);
+  const [espacios, setEspacios] = useState([]);
+  const [selectedDivId, setSelectedDivId] = useState(null);
+
+  const handleStartDateChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndDateChange = (date) => {
+    setEndDate(date);
+  };
+
+  const cerrarModal2 = () => {
+    setModal2(false);
+  };
+
+  const abrirModal2 = () => {
+    setModal2(true);
+  };
 
   const isFutureDate = (date) => {
     const currentDate = new Date();
@@ -35,10 +73,6 @@ const Espacios = () => {
     }
   };
 
-  const [espacios, setEspacios] = useState([]);
-  const [selectedDivId, setSelectedDivId] = useState(null);
-  const [selectedDivState, setSelectedDivState] = useState(null);
-  const [colorSeleccionado, setColorSeleccionado] = useState("");
   useEffect(() => {
     fetch("https://proyectoparqueo.000webhostapp.com/public/api/espacios")
       .then(response => response.json())
@@ -46,59 +80,107 @@ const Espacios = () => {
       .catch(error => console.log(error));
   }, []);
 
-  function ManejarCambio(event) {
-    const id = event.target.options[event.target.selectedIndex].id;
-    setColorSeleccionado(event.target.value);
-    console.log(id)
-  }
   const handleDivClick = (event) => {
-    setSelectedDivId(event.target.id);
-    setSelectedDivState(event.target.getAttribute('data-estado'));
+    const idEspacio = event.target.id;
+    setSelectedDivId(idEspacio);
     mostrarModal();
   };
-  const handleColorChange = (newColor) => {
-    document.getElementById(selectedDivId).style.backgroundColor = newColor;
-  };
-
-  const handleButtonClick = () => {
-    if (colorSeleccionado === "espacio libre") {
-      handleColorChange("#D9D9D9");
-    } else if (colorSeleccionado === "espacio ocupado") {
-      handleColorChange("#EC1C24");
-    } else if (colorSeleccionado === "espacio reservado") {
-      handleColorChange("#6083A9");
-    } else if (colorSeleccionado === "espacio deshabilitado") {
-      handleColorChange("#000000");
-    }
-
-  };
+  const enviarDatos = (e) => {
+    e.preventDefault();
+    //aqui falta ruta para enviar los datos
+  }
 
   return (
     <body>
       <div id="miModal" class="modal" >
         <span class="cerrar" onClick={cerrarModal}>&times;</span>
         <p6>Espacio: {selectedDivId}</p6> <br /><br />
-        <p6>Estado: {selectedDivState}</p6><br /><br />
-        <p6>Nuevo Estado:</p6> <br />
         <br />
         <div>
           <Calendar onChange={onChange} value={value} tileClassName={tileClassName} onClickDay={handleDateClick} />
           {selectedDate && (
             <div className="modal1">
-              <h2>Modal for {selectedDate.toLocaleDateString()}</h2>
+              <h2>Reservas para: {selectedDate.toLocaleDateString()}</h2>
+              <button onClick={abrirModal2} >reservar</button>
+              {Modal2 && (
+                <div className="modal2">
+                  <button type="button" className="cerrar" onClick={cerrarModal2}>&times;</button>
+                  <div>
+                    <form className="formulario" onSubmit={enviarDatos}>
+                      <h2>Reservar</h2>
+                      <label htmlFor="nombre">Nombre:</label>
+                      <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+
+                      <label htmlFor="apellidos">Apellidos:</label>
+                      <input type="text" id="apellidos" value={apellidos} onChange={(e) => setApellidos(e.target.value)} required />
+
+                      <label htmlFor="ci">CI:</label>
+                      <input type="text" id="ci" value={ci} onChange={(e) => setCI(e.target.value)} required />
+
+                      <label htmlFor="rol">Rol:</label>
+                      <input type="text" id="rol" value={rol} onChange={(e) => setRol(e.target.value)} required />
+
+                      <label htmlFor="celular">Celular:</label>
+                      <input type="text" id="celular" value={celular} onChange={(e) => setCelular(e.target.value)} required />
+
+                      <label htmlFor="correo">Correo:</label>
+                      <input type="email" id="correo" value={correo} onChange={(e) => setCorreo(e.target.value)} required />
+
+                      <label htmlFor="placa">Placa:</label>
+                      <input type="text" id="placa" value={placa} onChange={(e) => setPlaca(e.target.value)} required />
+
+                      <label htmlFor="marca">Marca:</label>
+                      <input type="text" id="marca" value={marca} onChange={(e) => setMarca(e.target.value)} required />
+
+                      <label htmlFor="modelo">Modelo:</label>
+                      <input type="text" id="modelo" value={modelo} onChange={(e) => setModelo(e.target.value)} required />
+
+                      <label htmlFor="tipo">Tipo:</label>
+                      <select id="tipo" value={tipo} onChange={(e) => setTipo(e.target.value)} required>
+                        <option value="">Seleccionar tipo...</option>
+                        <option value="Sedán">toyota</option>
+                        <option value="SUV">nissan</option>
+                        <option value="Pickup">suzuki</option>
+                        <option value="Deportivo">otro</option>
+                      </select>
+                      <label>hora inicio:</label>
+                      <DatePicker
+                        selected={startDate}
+                        onChange={handleStartDateChange}
+                        minDate={new Date()}
+                        minTime={new Date().getHours() + ":" + new Date().getMinutes()}
+                        maxTime="23:59"
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                      />
+                      <br />
+                      <label>hora fin:</label>
+                      <DatePicker
+                        selected={endDate}
+                        onChange={handleEndDateChange}
+                        minDate={startDate}
+                        minTime={new Date().getHours() + ":" + new Date().getMinutes()}
+                        maxTime="23:59"
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="MMMM d, yyyy h:mm aa"
+                      />
+                      <button type="submit">Reservar</button>
+                    </form>
+                  </div>
+                </div>
+              )}
+              <br />
+
+
               <button onClick={() => setSelectedDate(null)}>Close</button>
             </div>
           )}
         </div>
-        <select className="select-dropdown" onChange={ManejarCambio}>seleccionar
-          <option selected>Seleccionar</option>
-          <option value="espacio libre" id="espaciolibre" >Espacio libre</option>
-          <option value="espacio ocupado" id="espacioocupado" >Espacio ocupado</option>
-          <option value="espacio reservado" id="espacioreservado" >Espacio reservado</option>
-          <option value="espacio deshabilitado" id="espaciodeshabilitado" >Espacio deshabilitado</option>
-        </select>
         <br />
-        <button className='button' onClick={handleButtonClick}>Guardar</button>
       </div>
       <div >
         <br />
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react';
-import './login.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./login.css";
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // handle form submission logic here
-  };
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.post("", { email, password })// aqui la ruta de usuario
+      .then(response => {
+        
+      })
+      .catch(error => {
+        setError(error.response.data.message);
+      });
+  }
 
   return (
-    <div className="login">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button type="submit">Log In</button>
+    <div className="login-page">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h1>Iniciar sesión</h1>
+        <div className="form-group">
+          <label htmlFor="email">
+            <i className="fas fa-envelope"></i>
+          </label>
+          <input type="email" id="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">
+            <i className="fas fa-lock"></i>
+          </label>
+          <input type="password" id="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <button type="submit">Iniciar sesión</button>
+        {error && <div className="error">{error}</div>}
       </form>
     </div>
   );
-};
+}
 
 export default Login;
